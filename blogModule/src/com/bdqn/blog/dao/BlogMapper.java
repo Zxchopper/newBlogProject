2ccb013f4ce916ac534736f687ba82a7abdc26d5
caicainb <1019254779@qq.com>
2018-01-09 19:13:47 +0800

    博客的增删改查

--- blogModule/src/com/bdqn/blog/dao/BlogMapper.java
+++ blogModule/src/com/bdqn/blog/dao/BlogMapper.java
@@ -3,10 +3,17 @@ package com.bdqn.blog.dao;
 import com.bdqn.blog.pojo.Blog;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface BlogMapper{
 
+	//增加博客
 	int insertBlog(Blog blog);
+	//修改博客
 	int updateBlog(Blog blog);
+	//删除博客
 	int deleteBlog(@Param("bid") Integer id);
+	//根据条件查询博客
+	List<Blog> selectAllBlog(@Param("uid") int uid,@Param("title") String title,@Param("pageNo") int pageNo,@Param("pageSize")int pageSize);
 
 }
