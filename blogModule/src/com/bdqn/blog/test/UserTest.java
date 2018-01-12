d7120c4c394380b4b08e549f7aeaf3fd5c079289
bananananannana <33211102+bananananannana@users.noreply.github.com>
2018-01-12 19:55:46 +0800

    没有任何代码更新

---
+++ blogModule/src/com/bdqn/blog/test/UserTest.java
@@ -0,0 +1,20 @@
+package com.bdqn.blog.test;
+
+import com.bdqn.blog.server.impl.UserServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class UserTest {
+    public static void main(String[] atf){
+        ApplicationContext ac=new ClassPathXmlApplicationContext("applicationContext-mybatis.xml");
+        UserServiceImpl bean = ac.getBean(UserServiceImpl.class);
+        try {
+            bean.getLoginUser("user","123");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
