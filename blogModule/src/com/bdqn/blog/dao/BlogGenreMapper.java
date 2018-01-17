dbb43ac84b620bcd2a87b394fe39cfb697f117e8
younger0317 <young_er_2008@163.com>
2018-01-17 09:57:16 +0800

    更新博客分类增删改

--- blogModule/src/com/bdqn/blog/dao/BlogGenreMapper.java
+++ blogModule/src/com/bdqn/blog/dao/BlogGenreMapper.java
@@ -1,6 +1,7 @@
 package com.bdqn.blog.dao;
 
 import com.bdqn.blog.pojo.BlogGenre;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -8,7 +9,12 @@ import java.util.List;
 public interface BlogGenreMapper {
     //获取博客所有分类
     List<BlogGenre> getBlogGenreAll();
-
+    //添加分类
     int insert(BlogGenre blogGenre);
+    //删除
+    int delete(@Param("id") Integer id);
+    //修改
+    int update(BlogGenre blogGenre);
+
 
 }
